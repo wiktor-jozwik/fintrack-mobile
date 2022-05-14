0020a153e47840bf4365f6d1b14009ec7f0d2f8f
Wiktor Jóźwik <wiktor.jozwik@besidethepark.com>
2022-05-14 14:19:14 +0200

    Fix binding in operation list

--- app/src/main/java/com/example/moneytracker/view/adapter/OperationAdapter.kt
+++ app/src/main/java/com/example/moneytracker/view/adapter/OperationAdapter.kt
@@ -1,10 +1,8 @@
 package com.example.moneytracker.view.adapter
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.example.moneytracker.R
 import com.example.moneytracker.databinding.OperatationBinding
 import com.example.moneytracker.service.model.Operation
 import java.time.format.DateTimeFormatter
@@ -12,33 +10,24 @@ import java.time.format.DateTimeFormatter
 class OperationAdapter(
         private val operations: List<Operation>
     ) : RecyclerView.Adapter<OperationAdapter.OperationViewHolder>() {
-    private lateinit var binding: OperatationBinding
-
-    inner class OperationViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+    inner class OperationViewHolder(val binding: OperatationBinding) : RecyclerView.ViewHolder(binding.root)
 
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int,
     ): OperationViewHolder {
-        val inflater = LayoutInflater.from(parent.context)
-        binding = OperatationBinding.inflate(inflater)
-
-        return OperationViewHolder(
-            LayoutInflater.from(parent.context).inflate(
-                R.layout.operatation,
-                parent,
-                false
-            )
-        )
+        val binding = OperatationBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return OperationViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: OperationViewHolder, position: Int) {
         val curOperation = operations[position]
-        holder.itemView.apply {
-            binding.textName.text = curOperation.name
-            binding.textCategory.text = curOperation.category.name
-            binding.textMoneyAmount.text = "${curOperation.moneyAmount} ${curOperation.currency.symbol}"
-            binding.textDate.text = curOperation.date.format(DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm"))
+
+        holder.binding.apply {
+            textName.text = curOperation.name
+            textCategory.text = curOperation.category.name
+            textMoneyAmount.text = "${curOperation.moneyAmount} ${curOperation.currency.symbol}"
+            textDate.text = curOperation.date.format(DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm"))
         }
     }
 
